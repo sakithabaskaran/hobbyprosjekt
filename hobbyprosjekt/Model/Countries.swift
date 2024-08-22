f94e02cf173bfc73cb13889dfde513d8c21904e1
sakitha <sakithabaskaran@gmail.com>
2024-08-22 11:17:17 +0200

    Tried to make changes in Countries.swift for the data to be read, but didn't succeed. Minor changes.

--- hobbyprosjekt/Model/Countries.swift
+++ hobbyprosjekt/Model/Countries.swift
@@ -1,72 +1,71 @@
 // This file was generated from JSON Schema using codebeautify, do not modify it directly.
 // To parse the JSON, add this file to your project and do:
 //
-//   let welcome7 = try Welcome7(json)
+// let countriesJSON = try Countries(json)
 
 import Foundation
 
 // MARK: - Countries
-struct Countries: Codable, Identifiable {
+struct Countries: Decodable, Identifiable {
     var id: UUID = UUID()
     let name: Name
-    let tld: [String]?
+    let tld: [String]
     let cca2: String
     let ccn3: String
     let cca3: String
-    let independent: Bool?
-    let status: String
+    let independent: Bool
+    let status: Status
     let unMember: Bool
-    let currencies: [String: Currency]
+    let currencies: [String: Currency]?
     let idd: Idd
-    let capital: [String]?
+    let capital: [String]
     let altSpellings: [String]
-    let region: String
-    let languages: [String: String]?
+    let region: Region
+    let languages: [String: String]
     let translations: [String: Translation]
     let latlng: [Double]
     let landlocked: Bool
     let area: Double
-    let demonyms: [String: Demonym]
+    let demonyms: Demonyms
     let flag: String
     let maps: Maps
     let population: Int
-    let fifa: String?
     let car: Car
     let timezones: [String]
     let continents: [Continent]
     let flags: Flags
     let coatOfArms: CoatOfArms
-    let startOfWeek: String
+    let startOfWeek: StartOfWeek
     let capitalInfo: CapitalInfo
-    let cioc, subregion: String?
+    let cioc, subregion, fifa: String?
     let borders: [String]?
     let gini: [String: Double]?
     let postalCode: PostalCode?
 }
 
 // MARK: - CapitalInfo
-struct CapitalInfo: Codable {
-    let latlng: [Double]?
+struct CapitalInfo: Decodable {
+    let latlng: [Double]
 }
 
 // MARK: - Car
-struct Car: Codable {
-    let signs: [String]?
+struct Car: Decodable {
+    let signs: [String]
     let side: Side
 }
 
-enum Side: String, Codable {
-    case sideLeft
-    case sideRight
+enum Side: String, Decodable {
+    case left
+    case right
 }
 
 // MARK: - CoatOfArms
-struct CoatOfArms: Codable {
-    let png: String?
-    let svg: String?
+struct CoatOfArms: Decodable {
+    let png: String
+    let svg: String
 }
 
-enum Continent: String, Codable {
+enum Continent: String, Decodable {
     case africa
     case antarctica
     case asia
@@ -76,62 +75,58 @@ enum Continent: String, Codable {
     case southAmerica
 }
 
-// MARK: - Currencies
-struct Currency: Codable {
+// MARK: - Currency
+struct Currency: Decodable {
     let name, symbol: String
 }
 
 // MARK: - Demonyms
-struct Demonym: Codable {
+struct Demonyms: Decodable {
     let eng: Eng
     let fra: Eng?
 }
 
 // MARK: - Eng
-struct Eng: Codable {
+struct Eng: Decodable {
     let f, m: String
 }
 
 // MARK: - Flags
-struct Flags: Codable {
+struct Flags: Decodable {
     let png: String
     let svg: String
     let alt: String?
 }
 
 // MARK: - Idd
-struct Idd: Codable {
+struct Idd: Decodable {
     let root: String
-    let suffixes: [String]?
+    let suffixes: [String]
 }
 
 // MARK: - Maps
-struct Maps: Codable {
+struct Maps: Decodable {
     let googleMaps, openStreetMaps: String
 }
 
 // MARK: - Name
-struct Name: Codable {
+struct Name: Decodable {
     let common, official: String
-    let nativeName: Eng1
-}
-
-struct Eng1: Codable {
-    let common, official: String
+    let nativeName: [String: Translation]?
 }
 
 // MARK: - Translation
-struct Translation: Codable {
+struct Translation: Decodable {
     let official, common: String
 }
 
 // MARK: - PostalCode
-struct PostalCode: Codable {
+struct PostalCode: Decodable {
     let format: String
     let regex: String?
 }
 
-enum Region: String, Codable {
+enum Region: String, Decodable {
     case africa
     case americas
     case antarctic
@@ -140,9 +135,13 @@ enum Region: String, Codable {
     case oceania
 }
 
-enum StartOfWeek: String, Codable {
+enum StartOfWeek: String, Decodable {
     case monday
     case saturday
     case sunday
 }
 
+enum Status: String, Decodable {
+    case officiallyAssigned
+    case userAssigned
+}
