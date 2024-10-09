be7b6df8e29922a57f81f95c2e849019faa07962
sakitha <sakithabaskaran@gmail.com>
2024-10-09 23:11:53 +0200

    Made a new testing folder for unit tests.

---
+++ hobbyprosjektTests/hobbyprosjektTests.swift
@@ -0,0 +1,16 @@
+//
+//  hobbyprosjektTests.swift
+//  hobbyprosjektTests
+//
+//  Created by Sakitha Baskaran on 09/10/2024.
+//
+
+import Testing
+
+struct hobbyprosjektTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
